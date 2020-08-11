727c0dda045c7ff7851f5c3df35ff796d87043f9
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-08-12 05:42:25 +0600

    applyvscall

--- bleh.js
+++ bleh.js
@@ -12,5 +12,23 @@ const personName = {
     }
 }
 
-personName.salaryBill(3000);
-console.log(personName.salary);
+const coolDude = {
+    firstName : "Sarfaraz",
+    lastName : "Islam",
+    salary : 10000
+}
+const worseDude = {
+    firstName : "Ashik",
+    lastName : "Banaya",
+    salary : 10000
+}
+
+// const heroChargeBill = personName.salaryBill.bind(coolDude);
+// heroChargeBill (2000);
+// heroChargeBill(1500);
+// console.log(coolDude.salary);
+// console.log(personName.salary);
+
+
+normalPerson.chargeBill.call(heroPerson, 900); 
+console.log(heroPerson.salary);
--- callApply.js
+++ callApply.js
@@ -25,11 +25,17 @@ const friendlyPerson = {
 }
 // normalPerson.chargeBill();
 
- const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
- heroBillCharge(2000);
- heroBillCharge(5000);
- console.log(heroPerson.salary);
- console.log(normalPerson.salary);
+//  const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+//  heroBillCharge(2000);
+//  heroBillCharge(5000);
+//  console.log(heroPerson.salary);
+//  console.log(normalPerson.salary);
 
- const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
- friendlyChargeBill(2323);
+//  const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+//  friendlyChargeBill(2323);
+// normalPerson.chargeBill.call(heroPerson, 900, 100, 10); 
+// normalPerson.chargeBill.call(heroPerson, 5000, 30, 500);
+// console.log(heroPerson.salary);  
+// normalP 
+normalPerson.chargeBill.apply(heroPerson, [ 7000, 400, 600]);
+console.log(heroPerson.salary);
