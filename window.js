a2188eb964073aa1a8d9815b60e4ec2c4fcf6999
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-08-12 19:15:13 +0600

    window

---
+++ window.js
@@ -0,0 +1,10 @@
+var name =" Karim ";
+
+function add(num1, num2) {
+    var result = num1 + num2;
+    console.log("Result inside ", result);
+    return result;
+}
+
+var sum = add(13, 45);
+console.log(sum);
