b8d32206d0121b8e85aecfe3694e05eb00a4a73a
tamimkarim <67522154+tamimkarim@users.noreply.github.com>
2020-08-13 18:28:05 +0600

    timeout

---
+++ timeout.js
@@ -0,0 +1,20 @@
+function doSomething() {
+    console.log(3333);
+}
+// console.log(2222);
+setTimeout(function () {
+    console.log('I')
+}, 700);
+setTimeout(function () {
+    console.log('Am')
+}, 800);
+setTimeout(function () {
+    console.log('So')
+}, 1000);
+setTimeout(function () {
+    console.log('SO')
+}, 1100);
+setTimeout(function () {
+    console.log('Bored')
+}, 1300);
+// console.log(4444);
